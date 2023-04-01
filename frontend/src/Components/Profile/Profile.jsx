564bc476f8eedd7665177874e08027a7181ce7cf
Vinay Agarwal <vinayagarwal360@gmail.com>
2023-04-02 00:55:13 +0530

    Added profile route

--- frontend/src/Components/Profile/Profile.jsx
+++ frontend/src/Components/Profile/Profile.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
-import Card from './MarketCard';
+import Card from './Card';
 import SkeletonCard from '../SkeletonCard/SkeletonCard';
 import { useStateContext } from '../../Contexts/Context';
 import { useQuery } from 'urql';
+import { useParams } from 'react-router-dom';
 
 const MarketQuery = (owner_address) => {
   return `
-  query {
-    arts(where : {to: "${process.env.REACT_APP_CONTRACT_ADDRESS}"  from_not: "${owner_address}" }) {
-      id
-      from
-      to
-      tokenURI
-      price
+    query {
+      arts(where : {from: "0x0000000000000000000000000000000000000000" to: "${owner_address}" }) {
+        id
+        from
+        to
+        tokenURI
+        price
+      }
     }
-  }
-`;
+  `;
 };
 
-const Market = () => {
+const Profile = () => {
   const { account } = useStateContext();
+  const state = useParams();
   const [result, reexecuteQuery] = useQuery({
-    query: MarketQuery(account),
+    query: MarketQuery(state.address),
   });
 
   return (
@@ -29,9 +31,9 @@ const Market = () => {
       <div>
         <div className="text-gray-100 font-bold text-2xl tracking-wider sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl sm:leading-none pl-10 pt-10 mt-10">
           <h1 className="inline-block relative">
-            <span className="relative z-10">Market</span>
+            <span className="relative z-10">Profile</span>
             <span className="absolute top-0 left-0 z-0 text-blue-400 opacity-50 blur-xl">
-              Market
+              Profile
             </span>
           </h1>
         </div>
@@ -48,11 +50,15 @@ const Market = () => {
               <SkeletonCard />
             </>
           )}
-          {result.error && <div>Some error Occured</div>}
+          {result.error && console.log(result.error)}
           {!result.fetching &&
             !result.error &&
             result.data.arts.map((art, index) => (
-              <Card key={index} art={art} />
+              <Card
+                key={index}
+                art={art}
+                isOwner={account == state.address.toLowerCase() ? 'true' : null}
+              />
             ))}
         </div>
       </div>
@@ -60,4 +66,4 @@ const Market = () => {
   );
 };
 
-export default Market;
+export default Profile;
